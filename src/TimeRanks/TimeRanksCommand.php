0c07168f7e96ac41ae89b7f2e9c63fdbc58b2177
TheFixerDevelopment <30442910+TheFixerDevelopment@users.noreply.github.com>
2017-09-29 14:28:25 +0100

    Updated to our own version of the plugin.

--- src/TimeRanks/TimeRanksCommand.php
+++ src/TimeRanks/TimeRanksCommand.php
@@ -13,8 +13,8 @@ class TimeRanksCommand{
 
     public function run(CommandSender $sender, array $args) : bool{
         if(!isset($args[0])){
-            $sender->sendMessage("TimeRanks plugin by luca28pet");
-            $sender->sendMessage("Use /tr check ".($sender instanceof Player ? "[player]" : "<player>"));
+            $sender->sendMessage("§5(Help page)§6§l>>>>>> §r§aTimeRanks plugin by Zeao §5(Help page)§6§l<<<<<<§r\n§a/ranks check §2- Checks your timed rank.\n§a/ranks check <player> §2- Checks another player's rank.\n§cTimedRanks help page, made by Zeao.");
+            $sender->sendMessage("§7Please use: §e/ranks check ".($sender instanceof Player ? "[player]" : "<player>"));
             return true;
         }
         $sub = array_shift($args);
@@ -22,29 +22,29 @@ class TimeRanksCommand{
             case "check":
                 if(isset($args[0])){
                     if(!$this->plugin->getServer()->getOfflinePlayer($args[0])->hasPlayedBefore()){
-                        $sender->sendMessage("Player ".$args[0]." never played on this server");
+                        $sender->sendMessage("Player ".$args[0]." §chas never played on §6Void§bFactions§cPE");
                         return true;
                     }
                     if(!$this->plugin->data->exists(strtolower($args[0]))){
-                        $sender->sendMessage($args[0]." has played less than 1 minute on this server");
-                        $sender->sendMessage("Rank is: ".$this->plugin->default);
+                        $sender->sendMessage($args[0]." §ahas played less than 1 minute on §6Void§bFactions§cPE");
+                        $sender->sendMessage("§bTheir Rank is: ".$this->plugin->default);
                         return true;
                     }
-                    $sender->sendMessage($args[0]." has played ".$this->plugin->data->get(strtolower($args[0]))." minutes on this server");
-                    $sender->sendMessage("Rank is: ".$this->plugin->getRank(strtolower($args[0])));
+                    $sender->sendMessage($args[0]." §ahas played§2 ".$this->plugin->data->get(strtolower($args[0]))." §aminutes on §6Void§bFactions§cPE");
+                    $sender->sendMessage("§bTheir Rank is: ".$this->plugin->getRank(strtolower($args[0])));
                     return true;
                 }
                 if(!$this->plugin->data->exists(strtolower($sender->getName()))){
                     if(!($sender instanceof Player)){
-                        $sender->sendMessage("Please use /tr check <playername>");
+                        $sender->sendMessage("§7Please use: §e/ranks check <playername> §6To check another player's rank.\n§7Please use: §e/ranks check §6To check your own rank.");
                         return true;
                     }
-                    $sender->sendMessage("You have played less than 1 minute on this server");
-                    $sender->sendMessage("Rank is: ".$this->plugin->default);
+                    $sender->sendMessage("§a§lYou have played less than 1 minute ago on §6Void§bFactions§cPE");
+                    $sender->sendMessage("§r§bYour rank is: ".$this->plugin->default);
                     return true;
                 }
-                $sender->sendMessage("You have played ".$this->plugin->data->get(strtolower($sender->getName()))." minutes on this server");
-                $sender->sendMessage("Rank is: ".$this->plugin->getRank(strtolower($sender->getName())));
+                $sender->sendMessage("§b§lYou have played§r§3 ".$this->plugin->data->get(strtolower($sender->getName()))." §b§lminutes on §6Void§bFactions§cPE");
+                $sender->sendMessage("§a§lYour rank is: ".$this->plugin->getRank(strtolower($sender->getName())));
                 return true;
             break;
             default:
